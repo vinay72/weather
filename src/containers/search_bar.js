a4fe3ae86c6223094500b871910b75f0b95fc431
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-26 09:29:19 +0530

    Update search_bar.js

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 
 export default class SearchBar extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = {term: ''};
+		
+	}
+	
 	render(){
 		return(
         <form className = "input-group">
