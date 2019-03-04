da1ce35d1e71c590a42c71c3179623773a1be445
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-05 00:19:46 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
  const API_KEY = '522842a3d38cdd9163b4126f3f669250';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+ export function fetchweather(){
+ 	return{
+ 		type: FETCH_WEATHER
+ 	};
+
+ }
