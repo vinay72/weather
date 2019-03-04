46f2f8361e2fba4f45fa2f67bacbf5a7b806d461
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-05 00:32:20 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
  const API_KEY = '522842a3d38cdd9163b4126f3f669250';
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?id=${API_KEY}`;
+
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
  export function fetchweather(){
