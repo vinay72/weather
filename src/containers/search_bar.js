e1703631a5cfa16b35edbda7b1a271ba1d3f38c9
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-05 01:49:02 +0530

    Update search_bar.js

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 
 export default class SearchBar extends Component{
