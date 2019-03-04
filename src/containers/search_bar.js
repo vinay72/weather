7772998a1406df8ff0ea212b5a304e83da2015a7
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-05 02:14:01 +0530

    Update search_bar.js

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -11,6 +11,7 @@ class SearchBar extends Component{
 		this.state = {term: ''};
 
 		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event){
