4b42351d2154b195e867e240d8165b85e678ae5d
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-05 15:39:36 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@
  export const FETCH_WEATHER = 'FETCH_WEATHER';
 
  export function fetchWeather(city){
- 	const url = '${ROOT_URL}&q=${city}, us';
+ 	const url = `${ROOT_URL}&q=${city}, us`;
  	const request = axios.get(url);
        
  	return{
