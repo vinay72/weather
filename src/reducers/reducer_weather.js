7cc7cb8be642d8a1f43b15f935aed72b29d0cbaa
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-06 19:21:40 +0530

    Update reducer_weather.js

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,6 +3,6 @@ import { FETCH_WEATHER } from '../action/index';
 export default function(state = [], action){
 	switch (action.type){
 	case FETCH_WEATHER:
-		return [ action.payload.data];
+		return [action.payload.data, ...state]; // [ city, city, city]
     }
 } 
