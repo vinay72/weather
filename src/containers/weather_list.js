1e7022a78d01e74b5378bd31fd4837118858e614
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-07 18:58:23 +0530

    Update weather_list.js

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import connect form 'react-redux';
 
 export default class WeatherList extends Component{
 	render(){
 		return(
           <table className = "table table-hover">
             <thead>
-              <tr>
-              <th>City</th>
-	       <th>Temperature</th>
-               <th>Pressure</th>
-               <th>Humidity</th>		
-              </tr>
+                <tr>
+                  <th>City</th>
+                  <th>Temperature</th>
+                  <th>Pressure</th>
+                  <th>Humidity</th>
+                </tr>
             </thead>
             <tbody>
-             
             </tbody>
           </table>
 		);
 	}
 } 
+
+function mapStateToProps(state){
+	return { weather: state.weather }; 
+}
