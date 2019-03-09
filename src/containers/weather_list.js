15d8999f383f2daa02d1fa17608a285a7d911660
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-09 16:05:04 +0530

    Update weather_list.js

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import connect from 'react-redux';
+import { connect } from 'react-redux';
 
 class WeatherList extends Component{
 	renderWeather(cityData) {
