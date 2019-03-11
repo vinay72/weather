8c6d3271a7d58acf2627f306cbcc2c330e25934b
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-11 16:23:09 +0530

    Update weather_list.js

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,6 +5,7 @@ class WeatherList extends Component{
 	renderWeather(cityData) {
 		const name = cityData.city.name;
 		const temps = cityData.list.map(weather => weather.main.temp) 
+		 console.log(temps);
         return(
      	  <tr key = {name}>
      	    <td>{name}</td>
