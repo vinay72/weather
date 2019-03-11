9db0922619760e06dd668772994caa39c4525f09
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-11 21:53:13 +0530

    Update weather_list.js

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -12,9 +12,9 @@ class WeatherList extends Component{
         return (
      	  <tr key = {name}>
      	    <td>{name}</td>
-     	    <td>
-     	      <Chart data={temps} color="orange" />
-     	    </td>
+     	    <td><Chart data={temps} color="orange" /></td>
+     	    <td><Chart data={pressures} color="green" /></td>
+     	    <td><Chart data={humidities} color="black" /></td>
      	  </tr>
         );
 	}
