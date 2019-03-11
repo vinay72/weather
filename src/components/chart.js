1e1747b442182e27ccc897f34e6c1fecda06e387
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-11 22:03:57 +0530

    Update chart.js

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Sparklines, SparklinesLine }  from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesLineReferenceLine }  from 'react-sparklines';
 
 export default props => {
 	return(
         
     <div> 
-     <Sparklines height={120} width={180} data={props.data}>
+     <Sparklines height={50} width={80} data={props.data}>
         <SparklinesLine color = { props.color } /> 
+        <SparklinesLineReferenceLine type = "avg" />
      </Sparklines>
     </div>
 
