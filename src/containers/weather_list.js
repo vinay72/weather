0345c46e0bc9325752acd54c4ec24cd4f9907ca7
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-11 21:49:38 +0530

    Update weather_list.js

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,6 +6,8 @@ class WeatherList extends Component{
 	renderWeather(cityData) {
 		const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp); 
+	const pressures = cityData.list.map(weather => weather.main.pressure); 
+        const humidities = cityData.list.map(weather => weather.main.humidity);	
         
         return (
      	  <tr key = {name}>
