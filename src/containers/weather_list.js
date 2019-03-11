9627ec81be594e6158a211b83eeb46b158b69e3e
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-03-11 19:52:34 +0530

    Update weather_list.js

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import Chart from '../Components/chart';
 
 class WeatherList extends Component{
 	renderWeather(cityData) {
@@ -11,7 +11,7 @@ class WeatherList extends Component{
      	  <tr key = {name}>
      	    <td>{name}</td>
      	    <td>
-     	      
+     	      <Chart data={temps} color="orange" />
      	    </td>
      	  </tr>
         );
